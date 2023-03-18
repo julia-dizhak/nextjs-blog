401c39b1f19766dc73bbb7642233deb398e8d088
Julia Dizhak <julia.dizhak@archlet.ch>
2023-03-18 23:30:30 +0100

    get post data from markdown

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,6 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi, I am Julia. I am a software engineer and social influencer. You can contact me on twitter @juliadizhak </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
     </Layout>
   );
